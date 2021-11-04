3cef065c9b6a5343ef404d055a296a603db6c6e4
GMarx90 <grzegorz.marx@gmail.com>
2021-11-04 18:52:11 +0100

    próba

---
+++ Party/src/main/java/Party.java
@@ -0,0 +1,28 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class Party {
+
+
+    static void party(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true{
+            System.out.println("Wybierz opcje");
+            System.out.println("1. Wyświetl gości");
+            System.out.println("2. Dodaj gościa");
+            System.out.println("3. Wyświetl potrawy");
+            System.out.println("4. Znajdź po imieniu");
+            System.out.println("5. Wyjście");
+
+            int usherChoice= scanner.nextInt();
+
+            switch (usherChoice)
+            {
+                case 1-> System.out.println("Lista gości");
+
+            }
+        }
+
+    }
+}
---
+++ guest_list/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Wybierz opcje");
+        System.out.println("1. Wyświetl listę gości");
+        System.out.println("2. Dodaj gościa ");
+        System.out.println("3. Wyświetl potrwy ");
+        System.out.println("4. Znajdz po numerze telefonu ");
+        System.out.println("5. Znajdz po imieniu ");
+        System.out.println("6. Wyjdż ");
+        boolean decision= true;
+        while (decision) {
+            Scanner scanner = new Scanner(System.in);
+            int costumerChoice = scanner.nextInt();
+
+            switch (costumerChoice) {
+                case 1 -> System.out.println("Lista gości");
+                case 2 -> System.out.println("Dodaje gościa ");
+                case 3 -> System.out.println("Lista potraw ");
+                case 4 -> System.out.println("Znalazłem po numerze telefonu ");
+                case 5 -> System.out.println("Znalazłem po imieniu ");
+                case 6 -> decision=false;
+            }
+        }
+    }
+}
