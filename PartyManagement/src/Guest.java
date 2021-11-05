b9b49e88e06c53d0ec1c6f77af5eeb33ea02bb2b
GMarx90 <57858033+GMarx90@users.noreply.github.com>
2021-11-05 11:44:50 +0100

    Update Guest.java
    Third commit addguest method

--- PartyManagement/src/Guest.java
+++ PartyManagement/src/Guest.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Guest {
     private  String name;
     private  String meal;
     private  int phoneNumber;
     private boolean isVegan;
-
+Scanner scanner=new Scanner (System.in);
+    
     public Guest(String name, String meal, int phoneNumber, boolean isVegan) {
         this.name = name;
         this.meal = meal;
@@ -34,5 +37,26 @@ public void getGuestInformation(){
     String isVeganAnswer = isVegan ? "tak" : "nie";
     System.out.println("Czy jest veganinem: "+isVeganAnswer);
 }
-
+public void addGuest(){
+    System.out.println("Podaj imię: ")
+    name=scannner.nextLine();
+    System.out.println("Podaj potrawę: ")
+    meal= scannner.nextLine();
+    System.out.println("Podaj numer telefonu: ")
+    phoneNumber=scanner.nextInt();
+    System.out.println("Czy jest veganinem: Y/N");
+    isVeganAnswer=scanner.nextLine();
+    isVegan=false;
+    if (isVeganAnswer.equals("Y"){
+        isVegan=true}
+    if (isVeganAnswer.equals("N"){
+        isVegan=false}
+        else{
+             System.out.println("Zła odpowiedź")}
+        
+    String isVeganAnswer = isVegan ? "tak" : "nie";
+    System.out.println("Czy jest veganinem: "+isVeganAnswer);
+}
+    
+    
 }
